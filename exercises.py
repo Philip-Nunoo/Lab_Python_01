abbe2596e368fcaf940c00d5cde6bfa0535d4167
AITI Student <aiti-student@aiti-079-PC.(none)>
2012-06-20 07:44:52 -0400

    Answers to 01

---
+++ exercises.py
@@ -0,0 +1,52 @@
+print "Philip Afful Nunoo"
+
+j = "--add--"
+i = '3.14159'
+h = True
+g = '17'
+f = 17
+e = 'True'
+d = 7
+c = 'Alex'
+b = 3.7
+a = False
+print "a : ", type(a)
+print "b : ", type(b)
+print "c : ", type(c)
+print "d : ", type(d)
+print "e : ", type(e)
+print "f : ", type(f)
+print "g : ", type(g)
+print "h : ", type(h)
+print "i : ", type(i)
+print "j : ", type(j)
+
+print " "
+print "####################################"
+print "####################################"
+
+a = False
+b = True
+c = False
+
+print "1. b and c               : ", b and c
+print "2. b or c                : ", b or c
+print "3. not a and b           : ", not a and c
+print "4. (a and b) or not c    : ", (a and b) or not c
+print "5. not b and not (a or c): ", not b and not(a or c)
+print "6. not ((not b or not a) and c) or a : ", not ((not b or not a) and c) or a
+
+print " "
+print "####################################"
+print "####################################"
+
+print "1.  5/2       = " , 5/2
+print "2.  5/2.0     = " , 5/2.0
+print "3.  5.0/2     = " , 5.0/2
+print "4.  7*(1/2)   = " , 7*(1/2)
+print "5.  7*(1/2.0) = " , 7*(1/2.0)
+print "6.  5**2      = " , 5**2
+print "7.  5.0 **2   = " , 5.0 ** 2
+print "8.  5 ** 2.0  = " , 5 ** 2.0
+print "9.  1/3       = " , 1/3
+print "10. 1/3.0     = " , 1/3.0
